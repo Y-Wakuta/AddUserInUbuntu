be84c137a7ccdc200211756d296747b6ca88be18
yusuke <prsprivatestock@i.softbank.jp>
2018-09-18 17:16:07 +0900

    ファイルを追加

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AddUserInUbuntu
 ubuntuサーバでユーザを追加するためのscript
+
+## 使用方法
+
+管理者権限で実行する必要があるので、自己責任でお願いします。
+
+``` sh
+sudo ./adduser_ubuntu.sh $user_name $pub_key_path
+```
---
+++ adduser_ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#=======================================================
+#このスクリプトはあるユーザのhomeディレクトリに置かれることを想定する
+#引数1 user名
+#引数2 public keyファイルのpath
+#=======================================================
+set -e
+
+user_name=$1
+pub_key_path=$2
+
+script_dir="$(cd $(dirname $0); pwd)"
+useradd -m $user_name
+
+mkdir -p /home/$user_name/.ssh
+touch /home/$user_name/.ssh/authorized_keys
+echo $pub_key_path > /home/$user_name/.ssh/authorized_keys
+
+chown $user_name:$user_name /home/$user_name
+chown -R $user_name:$user_name /home/$user_name/.ssh
+
+chmod 755 /home/$user_name
+chmod -R 700 /home/$user_name/.ssh
